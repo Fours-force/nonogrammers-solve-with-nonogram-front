75184510c37f617bb6c1f68c8647659ee08015c1
YSHeee <scotchbroom0@kangwon.ac.kr>
2023-11-17 18:20:57 +0900

    chore: [SCRUM-100] set up initial Vue.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
-import App from './App.vue'
 import './assets/css/input.css'
 
-createApp(App).mount('#app')
+import App from './App.vue'
+import router from './router'
+
+import axios from 'axios'
+import { createApp } from 'vue'
+import { createPinia } from 'pinia' 
+
+axios.defaults.withCredentials = true;
+createApp(App).use(router).use(createPinia()).mount('#app')

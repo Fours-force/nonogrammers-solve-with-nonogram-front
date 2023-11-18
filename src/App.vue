9ae9ff98a61644c30806f650bf6cfba420a8811f
DESKTOP-TH36TT5\COM <babysoo123@gmail.com>
2023-11-18 20:57:10 +0900

    feat: [SCRUM-193] create nonodots router

--- src/App.vue
+++ src/App.vue
@@ -4,23 +4,11 @@ import TheWelcome from "./components/TheWelcome.vue";
 </script>
 
 <template>
-	<header>
-		<img
-			alt="Vue logo"
-			class="logo"
-			src="./assets/logo.svg"
-			width="125"
-			height="125"
-		/>
-
-		<div class="wrapper">
-			<HelloWorld msg="You did itii!" />
-		</div>
-	</header>
-
-	<main>
-		<TheWelcome />
-	</main>
+	<nav>
+    <router-link to="/nonodots">nonodots</router-link> |
+  	</nav>
+  <br />
+  <router-view />
 </template>
 
 <style scoped>

047354b889a9f703e36f6340efde143dd6f796a3
DESKTOP-TH36TT5\COM <babysoo123@gmail.com>
2023-11-20 14:09:09 +0900

    style: [SCRUM-193] progress bar 수정중

--- src/components/nono/nonodots.vue
+++ src/components/nono/nonodots.vue
@@ -6,7 +6,7 @@
                 <div class="w-full mt-4 bg-gray-50 rounded-full dark:bg-gray-700">
                     <div id="progressBar"
                          class="font-super256 text-white text-2xl font-medium bg-blue-600 text-center p-0.5 leading-none rounded-full memberCountCon"
-                         style="width: 1%;">
+                         :style="{width: progress}">
                         <div id="barPosition" class="block w-3 h-3">
                         </div>
                     </div>
@@ -31,32 +31,57 @@
                 <div id="ListbtnContainer" class="flex">
                     <div class="font-super256 min-w-max movebtn eightbit-btn" @click="check">CHECK
                     </div>
-                    <div class="font-super256 min-w-max movebtn eightbit-btn" @click=goToList>LIST
+                    <div class="font-super256 min-w-max movebtn eightbit-btn" @click="goToList">LIST
                     </div>
                 </div>
                 <br>
             </div>
-            <div class="font-super256 min-w-max movebtn eightbit-btn" @click=gonext>NEXT</div>
+            <div class="font-super256 min-w-max movebtn eightbit-btn" @click="gonext">NEXT</div>
         </div><br>
     </div>
 </template>
 
 <script setup>
 
-import {onMounted, watch, ref, onBeforeMount, computed} from 'vue';
-    import {setNonoData, data} from '@/assets/js/nono/nonodots';
-    import {api} from "@/assets/js/common";
+    import {onMounted, watch, ref, onBeforeMount, computed} from 'vue';
+    import {setNonoData} from '@/js/nono/nonodots';
+    import {api} from "@/js/common";
+    import {useRoute} from "vue-router";
 
-    let {progress, nonoId, urlAry, totalRowList, nonoClass} = setNonoData();
+    let {progress, nonoId, urlAry, totalRowList} = setNonoData();
+    const currentRoute = useRoute();
+    const userId = currentRoute.params.userId;
+    const baekjoonId = currentRoute.params.baekjoonId;
+    const routeNonoId = currentRoute.params.nonoId;
 
+    const check = () => {
+        console.log("check start !!!" )
+        api(`http://localhost:8089/api/updateCheck/${baekjoonId}/${userId}/${routeNonoId}`,'GET','').then((result) => {
+            console.log(result);
+            insertDots();
+        }).catch((e) => {
+            console.log(e);
+        });
+    };
+    const insertDots = () => {
+        console.log("insertDots start !!!")
+        var jsonData = {"solvingRow":solvingRow.value-1};
+        api(`http://localhost:8089/api/updateUserDot/${userId}/${routeNonoId}`,'POST',jsonData).then((result) => {
+            location.reload();
+
+        }).catch((e) => {
+          console.log(e);
+        });
+    }
     const getNono = () => {
       console.log("getNonodots start !!!");
-      api(`http://localhost:8089/nonodots/${data.userId}/${data.nonoId}/${data.baekjoonId}`,'Get','').then((result) => {
+      console.log(`userId : ${userId}, nonoId : ${routeNonoId}, beakjoonId : ${baekjoonId}`)
+      api(`http://localhost:8089/nonodots/${userId}/${routeNonoId}/${baekjoonId}`,'Get','').then((result) => {
         console.log(progress);
-        progress.value = ref(result.progress);
-        nonoId.value = ref(result.nonoId);
-        urlAry.value = ref(result.urlAry);
-        totalRowList.value = ref(result.totalRowList);
+        progress.value = result.progress;
+        nonoId.value = result.nonoId;
+        urlAry.value =result.urlAry;
+        totalRowList.value = result.totalRowList;
         console.log(progress);
         console.log(totalRowList);
         setStyle();
@@ -67,16 +92,14 @@ import {onMounted, watch, ref, onBeforeMount, computed} from 'vue';
       console.log("getNonodots end !!!");
     }
 
-
     const rowOnCheck = ref([]);
     const solvingRow = ref('0');
-    const newTotalRowList = ref([]);
-    let newSingleRowList = [];
-    let newSingleRowList2 = [];
+
     const rowMouseUp = (index) =>{
       console.log("mouseup");
+      console.log(progress);
       totalRowList.value[index].onRow = 'currentRowColor';
-      console.log(rowOnCheck[index]);
+      console.log(rowOnCheck);
     }
 
     const rowMouseOut = (index) => {
@@ -99,7 +122,7 @@ import {onMounted, watch, ref, onBeforeMount, computed} from 'vue';
     
     const loadSolvedDot = () =>{
         console.log("loadSolvedDot start!");
-        api(`http://localhost:8089/api/selectSolvedDotId/${data.userId}/${data.nonoId}`,'Get','').then((result) => {
+        api(`http://localhost:8089/api/selectSolvedDotId/${userId}/${routeNonoId}`,'Get','').then((result) => {
           console.log(result);
           let col = 0;
           let row = 0;
@@ -136,9 +159,7 @@ import {onMounted, watch, ref, onBeforeMount, computed} from 'vue';
     //     console.log("gonext");
     // };
 
-    // const check = () => {
-    //     console.log("check");
-    // };
+
 
     const goToList = () => {
         console.log("goToList");
@@ -148,17 +169,17 @@ import {onMounted, watch, ref, onBeforeMount, computed} from 'vue';
     const updateSolvingRow = (num) => {
       console.log("updateSolvingRow");
       solvingRow.value = num+1;
-      api(`http://localhost:8089/api/updateSolvingRow/${data.userId}/${data.nonoId}/${solvingRow.value}`,'GET','').then((result) =>{
-        console.log("앙 기모띠");
+      api(`http://localhost:8089/api/updateSolvingRow/${userId}/${routeNonoId}/${solvingRow.value}`,'GET','').then((result) =>{
+        console.log("줄 업데이트 완료");
       });
     }
 
     const getSolvingRow = () =>{
       console.log("getSolvingRow");
-      api(`http://localhost:8089/api/selectSolvingRow/${data.userId}/${data.nonoId}`,'GET','').then((result) =>{
+      api(`http://localhost:8089/api/selectSolvingRow/${userId}/${routeNonoId}`,'GET','').then((result) =>{
         console.log(result);
         solvingRow.value = result;
-        console.log("앙 기모띠");
+        console.log("줄 가져오기 완료");
       });
     }
 

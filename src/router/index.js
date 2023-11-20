986bef89604580f7edf7a584c62f4cb6d2661454
DESKTOP-TH36TT5\COM <babysoo123@gmail.com>
2023-11-20 14:09:39 +0900

    refactor: [SCRUM-193] 라우터 추가

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import nonodots from "../components/nono/nonodots.vue";
+import nonobox from "../components/nono/nonobox.vue"
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: "/nonodots", component: nonodots },
+    { path: "/nonodots/:userId/:nonoId/:baekjoonId", component: nonodots},
+    { path: "/nonobox", component: nonobox }
   ],
 });
 

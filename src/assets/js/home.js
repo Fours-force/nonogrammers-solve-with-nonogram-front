8e09974b0ec2c0b38e7f338c5161659d1a88c39a
DESKTOP-TH36TT5\COM <babysoo123@gmail.com>
2023-11-20 14:08:21 +0900

    refactor: [SCRUM-193] src/js로 이동

--- src/assets/js/home.js
+++
@@ -1,9 +0,0 @@
-// 프로필 dropdown 버튼
-function toggleDropdown(e){
-let dropdownElement = document.getElementById("dropdown");
-  if (dropdownElement.classList.contains('hidden')){
-    dropdownElement.classList.remove('hidden');
-  }else{
-    dropdownElement.classList.add('hidden');
-  }
-}
